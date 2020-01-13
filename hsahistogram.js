f6e492f02899899fc953927e4861051588198e8f
enemeth19 <erikanemeth99@gmail.com>
2020-01-13 05:51:33 -0500

    initial commit

---
+++ hsahistogram.js
@@ -0,0 +1,61 @@
+// Creating an array containing the health service areas (HSA) of all of the patients who had prostate cancer diagnoses
+const countHSA = []
+for (var i = 0; i < myData.length; i++) {
+    countHSA.push(myData[i].health_service_area)
+}
+
+// A checkpoint that returns an array containing the index of the patient and his/her HSA
+console.log(countHSA)
+
+// A function that returns an object with number of prostate cancer patients associated with each HSA
+function getHSAcounts(str, field) {
+    const HSACount = [];
+    for (var i = 0; i < myData.length; i++) {
+        if (myData[i][field] == str) {                 
+            if (HSACount[myData[i].health_service_area] == null)
+                HSACount[myData[i].health_service_area] = 1
+            else
+                HSACount[myData[i].health_service_area] += 1
+        }
+    }
+    return HSACount
+}
+
+// A checkpoint that returns an object with the number of diagnoses associated with patients whose HSA is New York City
+console.log(getHSAcounts('New York City', 'health_service_area'))
+
+function plotData(str, field) {
+    const dataToPlot = {
+        x: Object.keys(getHSAcounts(str, field)),
+        y: Object.values(getHSAcounts(str, field)),
+        type: 'bar'
+      };
+    return dataToPlot;
+}
+
+const getHSAhistogram = [plotData('Capital/Adirondack', 'health_service_area'),    
+    plotData('Central NY', 'health_service_area'),
+    plotData('Finger Lakes', 'health_service_area'),
+    plotData('Hudson Valley', 'health_service_area'),
+    plotData('Long Island', 'health_service_area'),
+    plotData('New York City', 'health_service_area'),
+    plotData('Southern Tier', 'health_service_area'),
+    plotData('Western NY', 'health_service_area')];
+
+const layout2 = {
+    title: 'Frequencies of Health Service Areas with Prostate Cancer Diagnoses',
+    font: {
+        family: 'Raleway, sans-serif'
+    },
+    showlegend: false,
+    xaxis: {
+        title: 'Health Service Area',
+        tickangle: -45
+    },
+    yaxis: {
+        title: 'Frequency',
+        gridwidth: 2
+    }
+  };
+
+Plotly.newPlot('hsahistogram', getHSAhistogram, layout2);
---
+++ racehistogram.js
@@ -0,0 +1,57 @@
+// Creating an array containing the races of all of the patients who had prostate cancer diagnoses
+const countRaces = []
+for (var i = 0; i < myData.length; i++) {
+    countRaces.push(myData[i].race)
+}
+
+// A checkpoint that returns an array containing the index of the patient and his/her race
+console.log(countRaces)
+
+// A function that returns an object with number of prostate cancer patients associated with each racial group
+function getRaceCounts(str, field) {
+    const raceCount = [];
+    for (var i = 0; i < myData.length; i++) {
+        if (myData[i][field] == str) {                 
+            if (raceCount[myData[i].race] == null)
+                raceCount[myData[i].race] = 1
+            else
+                raceCount[myData[i].race] += 1
+        }
+    }
+    return raceCount
+}
+
+// A checkpoint that returns an object with the number of diagnoses associated with patients who are Black/African American
+console.log(getRaceCounts('Black/African American', 'race'))
+
+function plotData(str, field) {
+    const dataToPlot = {
+        x: Object.keys(getRaceCounts(str, field)),
+        y: Object.values(getRaceCounts(str, field)),
+        type: 'bar'
+      };
+    return dataToPlot;
+}
+
+const getHistogram = [plotData('Black/African American', 'race'), 
+    plotData('Multi-racial', 'race'),     
+    plotData('Other Race', 'race'), // 'Other Race' includes Native Americans and Asian/Pacific Islander
+    plotData('White', 'race')];
+
+const layout = {
+    title: 'Frequencies of Races in Prostate Cancer Diagnoses',
+    font: {
+        family: 'Raleway, sans-serif'
+    },
+    showlegend: false,
+    xaxis: {
+        title: 'Racial Group',
+        tickangle: -45
+    },
+    yaxis: {
+        title: 'Frequency',
+        gridwidth: 2
+    }
+  };
+
+Plotly.newPlot('racehistogram', getHistogram, layout);
